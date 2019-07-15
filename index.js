1bcdf808919b7c2b188cd83f4e2d5bb45684fd20
KlNGU <brandynvay@yahoo.com>
2019-07-14 21:40:06 -0700

    tests are passing

--- index.js
+++ index.js
@@ -2,18 +2,4 @@
 
 const server = require('./lib/server.js');
 
-
-app.use((err, req, res, next) => {
-  console.log('Something went wrong, Error');
-  res.status(500);
-  res.send('Something went wrong, ERROR ヾ｜￣ー￣｜ﾉ')
-});
-
-app.use('*', (req, res, next) => {
-  console.log('Unknown Route');
-  res.status(404);
-  res.send('What is this ヽ(ﾟｰﾟ*ヽ)ヽ(*ﾟｰﾟ*)ﾉ(ﾉ*ﾟｰﾟ)ﾉ');
-  res.end();
-});
-
 server.start(8080);
